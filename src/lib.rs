b9e8e3da514a6cc10083cbbbf5a5750ba42bb414
Florian Klemme <mail@florianklemme.de>
2022-10-26 17:27:56 +0200

    Fixed doc test.

--- src/lib.rs
+++ src/lib.rs
@@ -5,11 +5,13 @@
 //! The Kueue package consists of multiple binary crates (client, selver, worker) to realize the distributed task scheduler.
 //! This library crate contains shared code between the Kueue binaries. To obtain the Kueue task scheduler, use "cargo install" instead:
 //!
-//! ```
+//! ```text
 //! cargo install kueue
 //! ```
 //!
 //! Find more information on [crates.io/crates/kueue](https://crates.io/crates/kueue).
+
+//#![warn(missing_docs)]
 
 pub mod config;
 pub mod messages;
